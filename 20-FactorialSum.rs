81c89d0ab3d6b48f3109464b8f083b2ad005611a
pseudobunny <ahsnap453@gmail.com>
2022-07-17 00:38:40 -0500

    Completed problem 20

---
+++ 20-FactorialSum.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // We don't need to calculate the factorial ourselves here - this is just a sum of the digits in the number
+    // If we do end up calculating the result of the factorial, the program would be essentially the same... just
+    // with a function result right here.
+    let factorial_result = "93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000";
+
+    println!("{}", factorial_result.chars().map(|c| c.to_digit(10).unwrap()).sum::<u32>())
+}
