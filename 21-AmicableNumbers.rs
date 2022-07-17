59bcbc6308ed0d399f1c1023f67c7b0707687fb8
pseudobunny <ahsnap453@gmail.com>
2022-07-17 12:05:16 -0500

    prior commit

--- 21-AmicableNumbers.rs
+++ 21-AmicableNumbers.rs
@@ -4,23 +4,20 @@ use std::collections::{
 };
 
 fn d(n: u64) -> u64 {
-    let mut max : u64 = n/2;
-    let mut divisor_sum: u64 = 1;
-    let mut i : u64 = 2;
-    loop {
-        if i >= max {
-            break;
+    let max: u64 = (n as f32).sqrt().ceil() as u64;
+    let mut sum: u64 = 1;
+    let mut i: u64 = 2;
+    while i < max {
+        if n%i == 0 {
+            sum += i + (n/i);
         }
-
-        if n % i == 0 {
-            max = n / i;
-            divisor_sum += i + max;
-        }
-
         i += 1;
     }
+    if i*i == n {
+        sum += i;
+    }
 
-    divisor_sum
+    sum
 }
 
 
@@ -36,10 +33,6 @@ fn main() {
             amicable_numbers.insert(d_result);
             amicable_numbers.insert(d_results[&d_result]);
         }
-    }
-
-    for an in &amicable_numbers {
-        println!("{}", an);
     }
 
     println!("{}", amicable_numbers.iter().sum::<u64>())
