1fd0aaddf71b2ad3c85c0bb06e43f47263ea0527
pseudobunny <ahsnap453@gmail.com>
2022-12-23 16:25:00 -0600

    formatted problem 90

--- 90-CubeDigitPairs/src/main.rs
+++ 90-CubeDigitPairs/src/main.rs
@@ -14,27 +14,28 @@ fn extend_set(set: &[u64]) -> Vec<u64> {
     copied_set
 }
 
-const SQUARES: [u64; 9] = [1,4,9,16,25,36,49,64,81];
-const SQUARE_DIGITS: [(u64,u64); 9] = [
-    (0,1),
-    (0,4),
-    (0,9),
-    (1,6),
-    (2,5),
-    (3,6),
-    (4,9),
-    (6,4),
-    (8,1)
+const SQUARES: [u64; 9] = [1, 4, 9, 16, 25, 36, 49, 64, 81];
+const SQUARE_DIGITS: [(u64, u64); 9] = [
+    (0, 1),
+    (0, 4),
+    (0, 9),
+    (1, 6),
+    (2, 5),
+    (3, 6),
+    (4, 9),
+    (6, 4),
+    (8, 1),
 ];
 
 enum OtherDigit {
     Required(u64),
     Optional(u64, u64),
-    Unviable
+    Unviable,
 }
 
 fn get_other_digits(n_v: &[u64]) -> Vec<OtherDigit> {
-    SQUARE_DIGITS.iter()
+    SQUARE_DIGITS
+        .iter()
         .map(|dt| {
             let c0 = n_v.contains(&dt.0);
             let c1 = n_v.contains(&dt.1);
@@ -55,52 +56,65 @@ fn get_other_digits(n_v: &[u64]) -> Vec<OtherDigit> {
 fn check_set_viable(n_v: &[u64]) -> bool {
     let odv = get_other_digits(&extend_set(n_v));
 
-    if odv.iter().any(|od| matches!(od,OtherDigit::Unviable)) {
+    if odv.iter().any(|od| matches!(od, OtherDigit::Unviable)) {
         return false;
     }
 
     let mut required = vec![];
     let mut optional = vec![];
-    for od in odv { 
+    for od in odv {
         match od {
             OtherDigit::Required(v) => required.push(v),
-            OtherDigit::Optional(v1, v2) => optional.push((v1,v2)),
-            _ => ()
+            OtherDigit::Optional(v1, v2) => optional.push((v1, v2)),
+            _ => (),
         };
     }
     // Filter down required
-    required = HashSet::<u64>::from_iter(required.iter().cloned()).iter().copied().collect();
+    required = HashSet::<u64>::from_iter(required.iter().cloned())
+        .iter()
+        .copied()
+        .collect();
 
     // Filter down optional
-    optional.dedup_by(|a,b| (a.0 == b.0 && a.1 == b.1) || (a.1 == b.0 && a.0 == b.1));
+    optional.dedup_by(|a, b| (a.0 == b.0 && a.1 == b.1) || (a.1 == b.0 && a.0 == b.1));
     let mut temp_optional = vec![];
     for pair in optional.iter() {
         // remove clear cases (aka one of the options is required)
-        let in_required = required.contains(&pair.0) || required.contains(&pair.1); 
+        let in_required = required.contains(&pair.0) || required.contains(&pair.1);
         if !in_required {
-             temp_optional.push(*pair);
+            temp_optional.push(*pair);
         }
     }
     optional = temp_optional;
 
     // Make optimal optional choices
-    let all_options = HashSet::<u64>::from_iter(optional.iter().flat_map(|t| once(t.0).chain(once(t.1))))
-        .iter()
-        .copied()
-        .collect::<Vec<u64>>();
+    let all_options =
+        HashSet::<u64>::from_iter(optional.iter().flat_map(|t| once(t.0).chain(once(t.1))))
+            .iter()
+            .copied()
+            .collect::<Vec<u64>>();
 
     let best_options = (1..=all_options.len())
         .flat_map(|n| all_options.iter().permutations(n).unique())
         .map(|v| v.iter().map(|n| **n).collect::<Vec<u64>>())
         .map(|v| extend_set(&v))
-        .find(|v| optional.iter().all(|p| v.contains(&p.0) || v.contains(&p.1)));
+        .find(|v| {
+            optional
+                .iter()
+                .all(|p| v.contains(&p.0) || v.contains(&p.1))
+        });
 
     match best_options {
         Some(v) => required.append(&mut v.to_vec()),
-        None => return false
+        None => return false,
     }
 
-    let len = required.len() - if required.contains(&6) && required.contains(&9) { 1 } else { 0 };
+    let len = required.len()
+        - if required.contains(&6) && required.contains(&9) {
+            1
+        } else {
+            0
+        };
 
     len <= 6
 }
@@ -109,21 +123,23 @@ fn check_if_sets_match(a: &[u64], b: &[u64]) -> bool {
     let a_e = extend_set(a);
     let b_e = extend_set(b);
 
-    let created_digits = a_e.iter()
-        .flat_map(|a_n| b_e.iter().map(|b_n| (*a_n, *b_n)).collect::<Vec<(u64, u64)>>())
-        .flat_map(|(a,b)| vec![a*10 + b, b*10 + a])
+    let created_digits = a_e
+        .iter()
+        .flat_map(|a_n| {
+            b_e.iter()
+                .map(|b_n| (*a_n, *b_n))
+                .collect::<Vec<(u64, u64)>>()
+        })
+        .flat_map(|(a, b)| vec![a * 10 + b, b * 10 + a])
         .collect::<Vec<u64>>();
 
     SQUARES.iter().all(|d| created_digits.contains(d))
 }
 
 fn main() {
-    let cube_sets = (0..10)
-        .combinations(6)
-        .unique();
+    let cube_sets = (0..10).combinations(6).unique();
 
-    let all_viable_sets = cube_sets
-        .filter(|s| check_set_viable(s));
+    let all_viable_sets = cube_sets.filter(|s| check_set_viable(s));
 
     let all_viable_pairs = all_viable_sets
         .combinations(2)
