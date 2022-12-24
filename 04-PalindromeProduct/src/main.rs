75f37cc2a99ba6b6042a6560d04c5f76e19d4f07
pseudobunny <ahsnap453@gmail.com>
2022-12-23 18:23:38 -0600

    moved problem 4

--- 04-PalindromeProduct/src/main.rs
+++ 04-PalindromeProduct/src/main.rs
@@ -5,21 +5,19 @@ fn check_palindrome(x: u32) -> bool {
     x_str.bytes().take(n).eq(x_str.bytes().rev().take(n))
 }
 
-fn main() {
+fn max_palindrome(min: u32, max: u32) -> u32 {
     let mut max_palindrome = 0;
 
-    let max_num = 1_000_000;
-    let max_factor = 1000;
-    let min_factor = 100;
+    let max_num = max * max;
     for i in 1..max_num {
         let curr_num = max_num - i;
-        
+
         if check_palindrome(curr_num) {
-            for j in 1..(max_factor - min_factor) {
-                let curr_factor = max_factor - j;
+            for j in 1..(max - min) {
+                let curr_factor = max - j;
                 let second_factor = curr_num / curr_factor;
-                
-                if (curr_num % curr_factor == 0) && (second_factor < max_factor && second_factor >= min_factor) {
+
+                if (curr_num % curr_factor == 0) && (second_factor < max && second_factor >= min) {
                     max_palindrome = curr_num;
                     println!("{}", curr_factor);
                     println!("{}", second_factor);
@@ -33,5 +31,24 @@ fn main() {
         }
     }
 
-    println!("{}", max_palindrome)
+    max_palindrome
+}
+
+fn main() {
+    println!("{}", max_palindrome(100, 1000))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn base_case() {
+        assert_eq!(max_palindrome(10, 100), 9009)
+    }
+
+    #[test]
+    fn q_case() {
+        assert_eq!(max_palindrome(100, 1000), 906609)
+    }
 }
