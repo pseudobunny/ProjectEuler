a595f6e5ec369cd076f79c66b6740a037d360e2a
pseudobunny <ahsnap453@gmail.com>
2023-01-02 12:45:09 -0600

    moved problem 20

--- 20-FactorialDigitSum/src/main.rs
+++ 20-FactorialDigitSum/src/main.rs
@@ -4,5 +4,11 @@ fn main() {
     // with a function result right here.
     let factorial_result = "93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000";
 
-    println!("{}", factorial_result.chars().map(|c| c.to_digit(10).unwrap()).sum::<u32>())
+    println!(
+        "{}",
+        factorial_result
+            .chars()
+            .map(|c| c.to_digit(10).unwrap())
+            .sum::<u32>()
+    )
 }
