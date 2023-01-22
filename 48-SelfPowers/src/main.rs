775455b292c1649ad00d7f85ad941e65b79b3ed1
pseudobunny <ahsnap453@gmail.com>
2023-01-22 17:09:25 -0600

    fixed up 48

--- 48-SelfPowers/src/main.rs
+++ 48-SelfPowers/src/main.rs
@@ -1,21 +1,35 @@
-use num_bigint::{BigUint, ToBigUint};
-use num_traits::Zero;
+use num::BigUint;
 
-fn main() {
-    let mut sum: BigUint = Zero::zero();
-
-    for i in 1..=1000 {
-        let ib = i.to_biguint().unwrap();
-        sum += ib.pow(i);
-    }
+fn last_ten_digits_of_d_to_pow_d_sum(max: u32) -> String {
+    let sum: BigUint = (1..=max).map(|i| BigUint::from(i).pow(i)).sum();
 
     // full big number
     let s = format!("{}", sum);
     // Get the last ten characters
-    let sub_s = s.chars()
-                 .skip(s.len()-10)
-                 .take(10)
-                 .collect::<String>();
+    s.chars().skip(s.len() - 10).take(10).collect::<String>()
+}
 
-    println!("{}", sub_s) 
+fn main() {
+    println!("{}", last_ten_digits_of_d_to_pow_d_sum(1000))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn base_case() {
+        assert_eq!(
+            last_ten_digits_of_d_to_pow_d_sum(10),
+            String::from("0405071317")
+        );
+    }
+
+    #[test]
+    fn q_case() {
+        assert_eq!(
+            last_ten_digits_of_d_to_pow_d_sum(1000),
+            String::from("9110846700")
+        );
+    }
 }
