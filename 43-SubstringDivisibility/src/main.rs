0de494ecc0b7fdb8ce26566af85106c426bd18c4
pseudobunny <ahsnap453@gmail.com>
2023-01-22 03:00:49 -0600

    refactored and formatted 43

--- 43-SubstringDivisibility/src/main.rs
+++ 43-SubstringDivisibility/src/main.rs
@@ -1,32 +1,41 @@
 use itertools::Itertools;
 
-fn can_divide(a: u64, b: u64) -> bool {
-    a % b == 0
-}
+const DIVISORS: [u64; 7] = [2, 3, 5, 7, 11, 13, 17];
+const DIGITS: [&str; 10] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 fn check_substring_divisibility(num: &str) -> bool {
-   let divisors = vec![2,3,5,7,11,13,17];
-   let substrings = num.chars()
-                       .collect::<Vec<char>>();
-   let substrings_nums = substrings.windows(3)
-                                   .skip(1)
-                                   .map(|vc| vc.iter().collect::<String>())
-                                   .map(|s| s.parse::<u64>().expect("Could not parse num"));
-   
-   substrings_nums.zip(divisors)
-                  .all(|(s_n, d)| can_divide(s_n, d))
+    num.chars()
+        .collect::<Vec<char>>()
+        .windows(3)
+        .skip(1)
+        .map(|vc| vc.iter().collect::<String>())
+        .map(|s| s.parse::<u64>().expect("Could not parse num"))
+        .zip(DIVISORS)
+        .all(|(s_n, d)| s_n % d == 0)
+}
+
+fn sum_of_all_substring_divisible() -> u64 {
+    DIGITS
+        .iter()
+        .copied()
+        .permutations(10)
+        .unique()
+        .map(|p| p.join(""))
+        .filter(|p| check_substring_divisibility(p))
+        .map(|p| p.parse::<u64>().expect("Could not parse num"))
+        .sum()
 }
 
 fn main() {
-    let digits = vec!["0","1","2","3","4","5","6","7","8","9"];
-    let sum: u64 = digits.iter()
-                         .copied()
-                         .permutations(digits.len())
-                         .unique()
-                         .map(|p| p.join(""))
-                         .filter(|p| check_substring_divisibility(p))
-                         .map(|p| p.parse::<u64>().expect("Could not parse num"))
-                         .sum();
+    println!("{}", sum_of_all_substring_divisible());
+}
 
-    println!("{}", sum);
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn q_case() {
+        assert_eq!(sum_of_all_substring_divisible(), 16695334890);
+    }
 }
