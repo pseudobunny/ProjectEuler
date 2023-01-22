624d171a264384fc222974451a565c5928071c15
pseudobunny <ahsnap453@gmail.com>
2023-01-22 02:54:51 -0600

    formatted 42

--- 42-CodedTriangleNumbers/src/main.rs
+++ 42-CodedTriangleNumbers/src/main.rs
@@ -1,22 +1,23 @@
+use custom_math_utilities::triangle_number_list;
 use std::{
     fs::File,
     io::{prelude::*, BufReader},
 };
-use custom_math_utilities::triangle_number_list;
 
 fn lines_from_file(filename: &str) -> Vec<String> {
-    let file = File::open(filename)
-        .expect("Something went wrong reading the file");
-    
+    let file = File::open(filename).expect("Something went wrong reading the file");
+
     let buf = BufReader::new(file);
-    
+
     buf.lines()
-       .map(|l| l.expect("Could not parse line").trim().to_string())
-       .collect()
+        .map(|l| l.expect("Could not parse line").trim().to_string())
+        .collect()
 }
 
 fn word_line_to_names(line: &str) -> Vec<String> {
-    line.split(',').map(|s| s.trim_matches('"').to_string()).collect()
+    line.split(',')
+        .map(|s| s.trim_matches('"').to_string())
+        .collect()
 }
 
 fn convert_word_to_sum(word: &str) -> u8 {
@@ -29,13 +30,13 @@ fn convert_word_to_sum(word: &str) -> u8 {
 
 fn triangle_names_in_file_count(filename: &str) -> usize {
     let input_lines = lines_from_file(filename);
-    
-    let mut tri_list = triangle_number_list(); 
-    
+
+    let mut tri_list = triangle_number_list();
+
     word_line_to_names(&input_lines[0])
-        .into_iter()             
+        .into_iter()
         .map(|w| convert_word_to_sum(&w) as u64) // convert to sum
-        .filter(|s| tri_list.is_in(*s)) // filter out words that aren't triangle numbers  
+        .filter(|s| tri_list.is_in(*s)) // filter out words that aren't triangle numbers
         .count()
 }
 
