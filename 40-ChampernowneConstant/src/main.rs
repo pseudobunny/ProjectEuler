57d1eee63b5010cd4a24481297ed939152a35127
pseudobunny <ahsnap453@gmail.com>
2023-01-22 01:55:29 -0600

    refactored 40

--- 40-ChampernowneConstant/src/main.rs
+++ 40-ChampernowneConstant/src/main.rs
@@ -1,28 +1,15 @@
+use custom_math_utilities::num_to_digits;
+
 // This is going to be a very brute force solution...
 // There's probably a smart way to count / figure this out, but the brute
 // force is so easy with the tools I've built for previous problems.
 
-fn digit_to_vec(num: u64) -> Vec<u64> {
-    fn push_inner(n: u64, digits: &mut Vec<u64>) {
-        digits.push(n % 10);
-
-        if n >= 10 {
-            push_inner(n/10, digits);
-        }
-    }
-    
-    let mut digits: Vec<u64> = vec![];
-    push_inner(num, &mut digits);
-
-    digits.into_iter().rev().collect()
-}
-
-fn main() {
+fn champernowne_constant() -> u64 {
     let mut collect: Vec<u64> = vec![];
 
     let mut n = 1;
     while collect.len() < 1_000_000 {
-        let mut d_vec = digit_to_vec(n);
+        let mut d_vec = num_to_digits(n).into_iter().rev().collect();
         collect.append(&mut d_vec);
         n += 1;
     }
@@ -30,9 +17,23 @@ fn main() {
     let mut p = 1;
     let mut t = 1;
     for _ in 0..7 {
-        p *= collect[t-1];
+        p *= collect[t - 1];
         t *= 10;
     }
 
-    println!("{}", p)
+    p
+}
+
+fn main() {
+    println!("{}", champernowne_constant())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn q_case() {
+        assert_eq!(champernowne_constant(), 210);
+    }
 }
