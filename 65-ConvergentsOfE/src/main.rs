f5273e11ce0a5f334f7219f2f21f5d14747d25b4
pseudobunny <ahsnap453@gmail.com>
2023-01-27 09:10:46 -0600

    refactored 65 with the the new abstracted convergents list

--- 65-ConvergentsOfE/src/main.rs
+++ 65-ConvergentsOfE/src/main.rs
@@ -1,58 +1,33 @@
-use num_bigint::{ToBigInt, BigInt};
-use num_traits::{Zero, One};
+use custom_math_utilities::{big_num_to_digits, ConvergentsList};
+use num::BigUint;
 
-struct ConvergentsList {
-    a: Vec<BigInt>,
-    h: Vec<BigInt>,
-}
-
-impl ConvergentsList {
-    fn new() -> ConvergentsList {
-        let a0 = 2.to_bigint().unwrap();
-        let a1: BigInt = One::one(); 
-
-        ConvergentsList {
-            a: vec![a0.clone(), a1.clone()],
-            h: vec![a0.clone(), a1*a0 + 1],
-        } 
-    }
-
-    fn extend(&mut self) {
-        let len_a: BigInt = self.a.len().to_bigint().unwrap();
-
-        let curr_a: BigInt = if (len_a.clone()+2) % 3 == One::one() {
-            (len_a+2)/3 * 2
+fn sum_of_numerator_e_continued_fraction(n: usize) -> BigUint {
+    let pattern = |i| {
+        if (i + 2) % 3 == 1 {
+            BigUint::from((i + 2) / 3 * 2)
         } else {
-            One::one()
-        };
-
-        self.a.push(curr_a.clone());
-        
-        let hk_len = self.h.len();
-
-        self.h.push(curr_a*self.h[hk_len-1].clone() + self.h[hk_len-2].clone());
-    }
-
-    fn get(&mut self, i: usize) -> (BigInt, BigInt) {
-        while i >= self.a.len() {
-            self.extend();
+            BigUint::from(1_u64)
         }
-        (self.a[i].clone(), self.h[i].clone())
-    }
-}
+    };
 
-fn sum_of_digits(mut n: BigInt) -> BigInt {
-    let mut sum = Zero::zero();
-    
-    while n != Zero::zero() {
-        sum += n.clone()%10;
-        n /= 10;
-    }
+    let mut e_conv = ConvergentsList::new(2, 1, pattern);
 
-    sum
+    big_num_to_digits(e_conv.get(n - 1).1).iter().sum()
 }
 
 fn main() {
-    let mut e_conv = ConvergentsList::new();
-    println!("{}", sum_of_digits(e_conv.get(99).1))
+    println!("{}", sum_of_numerator_e_continued_fraction(100))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn q_case() {
+        assert_eq!(
+            sum_of_numerator_e_continued_fraction(100),
+            BigUint::from(272_u64)
+        );
+    }
 }
