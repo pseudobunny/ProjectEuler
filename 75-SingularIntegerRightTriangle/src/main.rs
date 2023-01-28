37f990162e1cea5df6bee0e3c23236a2edda2fe1
pseudobunny <ahsnap453@gmail.com>
2023-01-27 21:35:40 -0600

    refactor 75

--- 75-SingularIntegerRightTriangle/src/main.rs
+++ 75-SingularIntegerRightTriangle/src/main.rs
@@ -1,16 +1,16 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 fn generate_pythagorean_triplets(n: u64) -> Vec<(u64, u64, u64)> {
     let mut triplets = vec![];
 
-    let mut triplet = (0,0,0);
+    let mut triplet = (0, 0, 0);
     let mut m = 2;
 
     while triplet.2 <= n {
         for n in 1..m {
-            triplet.0 = m*m - n*n;
-            triplet.1 = 2*m*n;
-            triplet.2 = m*m + n*n;
+            triplet.0 = m * m - n * n;
+            triplet.1 = 2 * m * n;
+            triplet.2 = m * m + n * n;
 
             triplets.push(triplet);
         }
@@ -21,7 +21,7 @@ fn generate_pythagorean_triplets(n: u64) -> Vec<(u64, u64, u64)> {
 }
 
 fn trip_sum(trip: (u64, u64, u64)) -> u64 {
-    trip.0+trip.1+trip.2
+    trip.0 + trip.1 + trip.2
 }
 
 fn sort_trip(trip: (u64, u64, u64)) -> (u64, u64, u64) {
@@ -31,42 +31,46 @@ fn sort_trip(trip: (u64, u64, u64)) -> (u64, u64, u64) {
     (t_v[0], t_v[1], t_v[2])
 }
 
-fn main() {
+fn num_single_trip_sets_under(n: u64) -> usize {
     // Generate all primitive triplets to which c is less than or equal to 1_500_000
-    let prim_triplets = generate_pythagorean_triplets(1_500_000);
-    
+    let prim_triplets = generate_pythagorean_triplets(n);
+
     let mut triplets = HashSet::new();
     for trip in prim_triplets.iter() {
-        let mut i = 1;
-        loop {
+        for i in 1.. {
             let new_trip = sort_trip((trip.0 * i, trip.1 * i, trip.2 * i));
-            
+
             if trip_sum(new_trip) > 1_500_000 {
                 break;
             }
 
             triplets.insert(new_trip);
-
-            i += 1;
         }
     }
-    
-    let trip_sums = triplets.iter().map(|t| trip_sum(*t)).collect::<Vec<u64>>();
 
-    let mut single_sums_set = HashSet::new();
-    let mut mul_sums_set = HashSet::new();
-    for t_s in trip_sums.iter() {
-        if mul_sums_set.contains(t_s) {
-            continue;
-        }
-        if single_sums_set.contains(t_s) {
-            single_sums_set.remove(t_s);
-            mul_sums_set.insert(t_s);
-            continue;
-        }
+    let trip_sums = triplets.iter().map(|t| trip_sum(*t));
 
-        single_sums_set.insert(t_s);
+    let mut trip_sum_by_count = HashMap::<u64, usize>::new();
+    for t_s in trip_sums {
+        trip_sum_by_count
+            .entry(t_s)
+            .and_modify(|s| *s += 1)
+            .or_insert(1);
     }
 
-    println!("{}", single_sums_set.len());
+    trip_sum_by_count.into_values().filter(|&c| c == 1).count()
+}
+
+fn main() {
+    println!("{}", num_single_trip_sets_under(1_500_000));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn q_case() {
+        assert_eq!(num_single_trip_sets_under(1_500_000), 161667);
+    }
 }
