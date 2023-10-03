b4729dab12107ef833d10d2e0c91121b0c14881a
pseudobunny <ahsnap453@gmail.com>
2023-10-03 18:31:34 -0500

    added struct to 62 solution

--- 62-CubicPermutations/src/main.rs
+++ 62-CubicPermutations/src/main.rs
@@ -3,6 +3,21 @@ use std::collections::HashMap;
 use custom_math_utilities::big_num_to_digits;
 use num::BigUint;
 
+struct CubePermutation {
+    value: BigUint,
+    permutation: String,
+}
+
+fn construct_cube_permutation(n: u64) -> CubePermutation {
+    let n_cube = BigUint::from(n) * BigUint::from(n) * BigUint::from(n);
+    let n_str = get_sorted_permutaiton_string(&n_cube);
+
+    CubePermutation {
+        value: n_cube,
+        permutation: n_str,
+    }
+}
+
 fn get_sorted_permutaiton_string(n: &BigUint) -> String {
     let mut n_digits = big_num_to_digits(n.clone());
 
@@ -14,21 +29,18 @@ fn get_sorted_permutaiton_string(n: &BigUint) -> String {
 fn smallest_cube_with_permutations(perm_target: usize) -> Option<BigUint> {
     let mut permutation_map = HashMap::<String, (usize, BigUint)>::new();
 
-    for n in 0_u64.. {
-        let n_cube = BigUint::from(n) * BigUint::from(n) * BigUint::from(n);
-        let n_str = get_sorted_permutaiton_string(&n_cube);
-
+    for cube in (0_u64..).map(construct_cube_permutation) {
         let new_value = permutation_map
-            .get(&n_str)
-            .map_or((1, n_cube.clone()), |(count, min_value)| {
-                (count + 1, std::cmp::min(min_value.clone(), n_cube))
+            .get(&cube.permutation)
+            .map_or((1, cube.value.clone()), |(count, min_value)| {
+                (count + 1, std::cmp::min(min_value.clone(), cube.value))
             });
 
         if new_value.0 >= perm_target {
             return Some(new_value.1);
         }
 
-        permutation_map.insert(n_str, new_value);
+        permutation_map.insert(cube.permutation, new_value);
     }
 
     None
