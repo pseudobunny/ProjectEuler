fbc0305a0707dc9504059d41108cc175d5bd1e0b
pseudobunny <ahsnap453@gmail.com>
2024-11-06 21:18:22 -0500

    use more sensible upper limit

--- 88-ProductSumNumbers/src/main.rs
+++ 88-ProductSumNumbers/src/main.rs
@@ -38,7 +38,7 @@ fn product_sets(x: usize) -> Vec<Vec<usize>> {
 fn find_minimal_product_sum_number_sum(k_max: usize) -> usize {
     let mut product_number_sum_map = HashMap::<usize, usize>::new();
 
-    for x in 4..k_max * 9 {
+    for x in 4..=k_max * 2 {
         product_sets(x)
             .iter()
             .filter(|set| set.iter().sum::<usize>() <= x)
